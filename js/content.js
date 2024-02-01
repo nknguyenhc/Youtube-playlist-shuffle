6b9d0df26a8a94a3441a0d83febc0bd39179ca12
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2024-02-01 17:55:50 +0800

    Attach event listener logic

--- js/content.js
+++ js/content.js
@@ -11,13 +11,35 @@ function getPlaylistItemNodes() {
   return Array.from(document.querySelectorAll('ytd-playlist-panel-video-renderer'));
 }
 
+function getContainerNode() {
+  return document.querySelector('#container.style-scope.ytd-player');
+}
+
+function getVideoNode(containerNode) {
+  return containerNode.querySelector('video.video-stream.html5-main-video');
+}
+
+function isAdVideo(containerNode) {
+  return containerNode.querySelector('.ytp-heat-map') === null;
+}
+
+function getVideoListener(videoNode) {
+  return () => {
+    if (videoNode.currentTime > videoNode.duration - 5) {
+      videoNode.pause();
+    }
+  }
+}
+
 function main() {
   const manager = getManager();
   if (!manager) {
+    // not in a Youtube playlist
     return false;
   }
   let elements = [];
-  const pageObserver = new MutationObserver(() => {
+  const playlistObserver = new MutationObserver(() => {
+    // shuffle upon detecting the list
     const newElements = getPlaylistItemNodes();
     if (elements.length === newElements.length || newElements.length === 0) {
       return;
@@ -25,12 +47,45 @@ function main() {
     elements = newElements;
     manager.shuffle(elements.length);
   });
-  pageObserver.observe(document, {
+  playlistObserver.observe(document, {
     childList: true,
     subtree: true,
   });
 
-  return () => pageObserver.disconnect();
+  let videoNode;
+  let videoController;
+  const attachEventListener = () => {
+    const containerNode = getContainerNode();
+    if (!containerNode) {
+      return;
+    }
+    if (isAdVideo(containerNode)) {
+      return;
+    }
+    // attach event listener
+    if (videoNode) {
+      return;
+    }
+    console.log("attaching event listener");
+    videoNode = getVideoNode(containerNode);
+    videoController = getVideoListener(videoNode);
+    videoNode.addEventListener('timeupdate', videoController);
+  };
+  attachEventListener();
+  const videoObserver = new MutationObserver(attachEventListener);
+  videoObserver.observe(document, {
+    attributes: true,
+    childList: true,
+    subtree: true,
+  });
+
+  return () => {
+    playlistObserver.disconnect();
+    videoObserver.disconnect();
+    if (videoNode) {
+      videoNode.removeEventListener('timeupdate', videoController);
+    }
+  };
 }
 
 function execute() {
