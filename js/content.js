7086ac786db86cfb42ef4b17feef1847c49b4083
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2024-02-02 19:35:45 +0800

    Improve autoshuffling
    Previously, in list view without video, it does not shuffle.
    Now, it shuffles, by extending the querySelector.

--- js/content.js
+++ js/content.js
@@ -8,7 +8,7 @@ function getManager() {
 }
 
 function getPlaylistItemNodes() {
-  return Array.from(document.querySelectorAll('ytd-playlist-panel-video-renderer'))
+  return Array.from(document.querySelectorAll('ytd-playlist-panel-video-renderer, ytd-playlist-video-renderer'))
     .filter(el => el.offsetParent !== null);
 }
 
